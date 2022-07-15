c6a50d2d48eeda85800d06d14dfeb2e2859800c1
Dan <dan.dobek@gmail.com>
2022-07-15 14:25:46 -0400

    added rounds and count

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -32,19 +32,24 @@ function RockPaperScissors (ComputerChoice,PlayerChoice){
 
     console.log(ComputerChoice);
     console.log(PlayerChoice);
+    
+    let Result;
 
     if(ComputerChoice=="rock"){
         
         if(PlayerChoice=="rock"){
             console.log("Tie");
+            Result = "tie";
         }
         
         if(PlayerChoice=="paper"){
             console.log("Player wins, paper beats rock!");
+            Result = "player";
         }
 
         if(PlayerChoice=="scissors"){
             console.log("Computer wins, rock beats scissors!");
+            Result = "computer";
         }
 
     }
@@ -53,14 +58,17 @@ function RockPaperScissors (ComputerChoice,PlayerChoice){
         
         if(PlayerChoice=="rock"){
             console.log("Computer wins, paper beats rock!");
+            Result = "computer";
         }
         
         if(PlayerChoice=="paper"){
             console.log("Tie");
+            Result = "tie";
         }
 
         if(PlayerChoice=="scissors"){
             console.log("Player wins, scissors beats paper!");
+            Result = "player";
         }
 
         
@@ -70,20 +78,44 @@ function RockPaperScissors (ComputerChoice,PlayerChoice){
                 
         if(PlayerChoice=="rock"){
             console.log("Player wins, rock beats scissors!");
+            Result = "player";
         }
         
         if(PlayerChoice=="paper"){
             console.log("Computer wins, scissors beats paper!");
+            Result = "computer";
         }
 
         if(PlayerChoice=="scissors"){
             console.log("Tie");
+            Result = "tie"
         }
+
+    }
+
+    return Result;
+}
+
+let Plyr = 0;
+let Cmptr = 0;
+
+for (let i=0; i<5;i++){
+    
+    let Round = RockPaperScissors(getComputerChoice(),getPlayerChoice());
+
+    if(Round=="computer"){
+        Cmptr++;
+    }
+    else if(Round=="player"){
+        Plyr++;
+    }
+    else if(Round=="tie"){
 
     }
 }
 
-RockPaperScissors(getComputerChoice(),getPlayerChoice());
+console.log(Plyr);
+console.log(Cmptr);
 
 
 
